5ebcdd54d79dda4348aaf1aefe37271c5070dce4
tomas pereira <tomaspereira2k@gmail.com>
2024-05-12 18:45:24 -0300

    menu mobile completado

--- src/Components/MobileMenu.jsx
+++ src/Components/MobileMenu.jsx
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 const MobileMenu = () => {
     return (
         <div className="absolute top-[4.5rem] flex justify-end w-full h-full bg-black/50">
-            <div className="grid gap-8 w-3/4 h-full p-4 bg-background1">
-                <div className="flex items-end p-6 bg-headerBg bg-cover rounded-md">
-                    <div className="flex flex-col ">
+            <div className="flex flex-col p-6 gap-8 w-3/4 bg-background1">
+                <div className={`${'md:flex'} hidden items-end p-6 bg-headerBg bg-cover rounded-lg`}>
+                    <div className="flex flex-col">
                         <p className="text-white font-bold">Frontend Mentor</p>
                         <p className="text-sm text-white font-bold">Feedback Board</p> 
                     </div>
                 </div>
-                <div className="flex flex-wrap gap-2 p-6 bg-white rounded-md">
-                    <button className="text-text2 text-sm px-2 py-1 font-bold bg-background1 rounded-md">All</button>
-                    <button className="text-text2 text-sm px-2 py-1 font-bold bg-background1 rounded-md">UI</button>
-                    <button className="text-text2 text-sm px-2 py-1 font-bold bg-background1 rounded-md">UX</button>
-                    <button className="text-text2 text-sm px-2 py-1 font-bold bg-background1 rounded-md">Enhancement</button>
-                    <button className="text-text2 text-sm px-2 py-1 font-bold bg-background1 rounded-md">Bug</button>
-                    <button className="text-text2 text-sm px-2 py-1 font-bold bg-background1 rounded-md">Feature</button>
+                <div className="flex flex-wrap gap-x-2 gap-y-4 p-6 bg-white rounded-lg">
+                    <button className="text-text2 text-sm px-4 py-1 font-bold bg-background1 rounded-lg">All</button>
+                    <button className="text-text2 text-sm px-4 py-1 font-bold bg-background1 rounded-lg">UI</button>
+                    <button className="text-text2 text-sm px-4 py-1 font-bold bg-background1 rounded-lg">UX</button>
+                    <button className="text-text2 text-sm px-4 py-1 font-bold bg-background1 rounded-lg">Enhancement</button>
+                    <button className="text-text2 text-sm px-4 py-1 font-bold bg-background1 rounded-lg">Bug</button>
+                    <button className="text-text2 text-sm px-4 py-1 font-bold bg-background1 rounded-lg">Feature</button>
                 </div>
-                <div className="flex flex-col gap-8 p-8 bg-white rounded-md">
+                <div className="flex flex-col gap-8 p-6 bg-white rounded-lg">
                     <div className="flex justify-between items-center">
                         <p className="font-bold">Roadmap</p>
                         <Link className="text-text2 hover:underline">View</Link>
--- src/Pages/FeedbackBoard.jsx
+++ src/Pages/FeedbackBoard.jsx
@@ -9,7 +9,7 @@ const FeedbackBoard = () => {
     return (
         <>
         <header className="flex justify-between items-center p-4 bg-mobileHeaderBg bg-cover">
-            <div className="">
+            <div>
                 <p className="text-white font-bold">Frontend Mentor</p>
                 <p className="text-xs text-white font-bold">Feedback Board</p>
             </div>
@@ -18,16 +18,18 @@ const FeedbackBoard = () => {
                 : <img src="./shared/mobile/icon-hamburger.svg" alt=" " />}
             </div>
         </header>
-        {mobileMenuIsOpen && createPortal(<MobileMenu /> ,document.getElementById("mobile-menu-portal"))}
-        <div className="flex justify-between items-center p-4 text-sm bg-background2">
-            <div className="flex items-center gap-2 w-fit">
-                <p className="text-white">Sort by: <span className="font-bold">{selectedSort}</span></p>
-                <img src="./shared/icon-arrow-down.svg" alt=" " />
+        {mobileMenuIsOpen && createPortal(<MobileMenu mobileMenuIsOpen={mobileMenuIsOpen}/> ,document.getElementById("mobile-menu-portal"))}
+        <main>
+            <div className="flex justify-between items-center p-4 text-sm bg-background2">
+                <div className="flex items-center gap-2 w-fit">
+                    <p className="text-white">Sort by: <span className="font-bold">{selectedSort}</span></p>
+                    <img src="./shared/icon-arrow-down.svg" alt=" " />
+                </div>
+                <Link className="px-4 py-2 bg-purple rounded-lg">
+                    <p className="text-white font-bold">+ Add Feedback</p>
+                </Link>
             </div>
-            <Link className="px-4 py-2 bg-purple rounded-lg">
-                <p className="text-white font-bold">+ Add Feedback</p>
-            </Link>
-        </div>
+        </main>
         </>
     )
 }
